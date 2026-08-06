eaef8afe5206f6210e42881941c73748c93ff128
aishwaryamekaran-a11y <aishwaryamekaran@gmail.com>
2026-08-06 09:37:43 +0530

    Add files via upload
    Week1 day2 class assignments and home assignments

---
+++ week1day2/week1Day2.js
@@ -0,0 +1,49 @@
+//FirstName
+/* var firstName="Test"
+console.log(firstName,typeof(firstName));
+var age="29"
+console.log(age,typeof(age));
+var companyName="TestLeaf"
+console.log(companyName);
+var address
+console.log(address,typeof(address));
+var mobileNumber=9876543210n
+console.log(mobileNumber,typeof(mobileNumber));
+var isAutomation=true
+console.log(isAutomation,typeof(isAutomation));
+var hasPlaywright=null
+console.log(hasPlaywright,typeof hasPlaywright);
+ */
+
+//String
+ let firstName="Ishwarya"
+console.log(firstName,typeof(firstName));
+
+//String
+let companyName="TestLeaf"
+console.log(companyName,typeof(companyName));
+
+//Number
+let mobileNumber=9876543210
+console.log(mobileNumber,typeof(mobileNumber));
+
+//Boolean
+let knowsAutomation=true
+console.log(knowsAutomation,typeof(knowsAutomation));
+
+//Undefined
+let usesPlaywright
+console.log(usesPlaywright,typeof usesPlaywright);
+ 
+
+
+
+
+ /* function print()
+{
+    let Num1=1
+    let Num2=2
+    console.log(Num1+Num2);
+}
+print() 
+ */
